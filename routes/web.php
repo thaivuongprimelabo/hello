1ed1542d6c3de9833a53b1401212f249eb035b6e
thanhbinh-primelabo <thanh.binh@primelabo.com.vn>
2018-04-18 13:20:41 +0700

    Update Code

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,28 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (Auth::check()) {
+        return view('welcome');
+    } else {
+        return redirect('/auth/login');
+    }
 });
 
 Auth::routes();
 
-Route::get('/login', function(){
-    return redirect('/auth/login'); 
+Route::get('/login', function() {
+    return redirect('/auth/login');
 });
 
 Route::get('/home', 'HomeController@index')->name('home');
