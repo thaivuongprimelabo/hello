f513cb5b254b3ba6a1d3839e0a3e68bdea65ee60
thanhbinh-primelabo <thanh.binh@primelabo.com.vn>
2018-04-19 17:37:48 +0700

    Login Require|min:3

--- resources/views/admin/login/login.blade.php
+++ resources/views/admin/login/login.blade.php
@@ -24,7 +24,7 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
-        <link rel="stylesheet" href="{{ URL::to('/admin/') }}/css/custom-styles.css">
+        <link rel="stylesheet" href="{{ URL::to('/assets/admin/') }}/css/custom-styles.css">
         <meta name="csrf-token" content="{{csrf_token()}}" />
         <!-- Google Font -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
